a67db5324fc9a23baa06e4dd4ac94da9cccf08d6
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-03-19 16:52:21 +0100

    filter schemas by current layer

--- js/extension/plugins/selectors.js
+++ js/extension/plugins/selectors.js
@@ -1,27 +1,14 @@
-import { responsesSelector } from '@mapstore/selectors/mapInfo';
+import { currentResponseSelector } from '@mapstore/selectors/mapInfo';
 import { createSelector } from "reselect";
 
 const schemaSelector = state => state.reportExtension && state.reportExtension.schemas || [];
 
-const responsesWithFeaturesSelector = createSelector(
-  responsesSelector,
-  responses => responses.filter((response) => {
-      if (response && response.layer && response.response !== "no features were found\n") {
-          return response;
-      }
-  })
-)
-
 export const schemasByLayersSelector = createSelector(
   schemaSelector,
-  responsesWithFeaturesSelector,
-  (schemas, responses) => schemas.filter(schema => {
-    let occurs = false;
-    responses.forEach((response) => {
-      if (schema.layer_id === response.layer.id) {
-        occurs = true;
-      }
-    });
-    return occurs;
+  currentResponseSelector,
+  (schemas, response) => schemas.filter(schema => {
+    if (response) {
+      return schema.layer_id === response.layer.id;
+    }
   })
 )
