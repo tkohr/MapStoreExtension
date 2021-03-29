637204709ede7f65de0391acb1d42997a3a47743
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-03-29 15:20:10 +0200

    use dev/prod API depending on env

--- js/extension/epics/epics.js
+++ js/extension/epics/epics.js
@@ -1,7 +1,7 @@
-import {loadedSchemas, loadError} from "../actions/actions";
-import {layersSelector} from 'mapstore2/web/client/selectors/layers'
+import { loadedSchemas, loadError } from "../actions/actions";
+import { layersSelector } from 'mapstore2/web/client/selectors/layers'
 import * as MapInfoUtils from 'mapstore2/web/client/utils/MapInfoUtils'
-import {extensionComponent} from '../plugins/Extension.jsx'
+import { extensionComponent } from '../plugins/Extension.jsx'
 import { updateNode } from 'mapstore2/web/client/actions/layers'
 import { updateFeatureInfoClickPoint } from "@mapstore/actions/mapInfo"
 import Rx from "rxjs";
@@ -12,58 +12,58 @@ import axios from '@mapstore/libs/ajax';
 
 
 export const fetchSchemasEpic = (action$, store) => action$.ofType('FETCH_SCHEMAS').switchMap(() => {
-        MapInfoUtils.setViewer('reportViewer', extensionComponent);
-        //TODO: make axios work with our API (fetch does) and use API
-        return Rx.Observable.defer(() => axios.get("https://georchestra.mydomain.org/mapstore-reports/reports"))
-            .switchMap((response) => Rx.Observable.of(loadedSchemas(/*response.data*/mockSchemas)))
-            .catch(e => Rx.Observable.of(loadError(e.message)));
+    // set custom viewer to inject extension component into Identify    
+    MapInfoUtils.setViewer('reportViewer', extensionComponent);
+    // use axios in prod, fetch in dev
+    // TODO: replace 'reports' API (used to test ping) by 'schemas' API once it exists
+    const fetchAPI = process.env.NODE_ENV === 'production' ?
+        axios.get('../mapstore-reports/reports')
+            .then(response => response.data) :
+        fetch('https://georchestra.mydomain.org/mapstore-reports/reports')
+            .then(response => response.json());
 
-        // //get data from API response (in dev)
-        // return Rx.Observable.fromPromise(
-        //         fetch('http://localhost:8080/report_models')
-        //             .then(response => response.json())
-        //     )
-        //     .switchMap((response) => {
-        //         return Rx.Observable.of(loadedSchemas(response))
-        //     })
-        //     .catch(e => Rx.Observable.of(loadError(e.message)));
+    // TODO: remove mocks and use API response
+    return Rx.Observable.of(loadedSchemas(mockSchemas));
+    // return Rx.Observable.fromPromise(fetchAPI)
+    //     .switchMap((response) => {
+    //         console.log('Fetched schemas: ', response)
+    //         return Rx.Observable.of(loadedSchemas(response))
+    //     })
+    //     .catch(e => Rx.Observable.of(loadError(e.message)));
+});
 
-    });
+export const displayFormEpic = (action$, store) => action$.ofType('DISPLAY_FORM').mergeMap(() => {
+    // no featureInfo = identify mode
+    const display = store.getState().reportExtension.display;
+    const featureInfo = (display) ? {
+        format: "PROPERTIES",
+        viewer: {
+            type: 'reportViewer'
+        }
+    } : undefined;
 
-export const   displayFormEpic = (action$, store) => action$.ofType('DISPLAY_FORM').mergeMap(() => {
-        
-        
-        // no featureInfo = identify mode
-        const display = store.getState().reportExtension.display;
-        const featureInfo = (display) ? {
-            format: "PROPERTIES",
-            viewer: {
-                type: 'reportViewer'
-            }
-        } : undefined;
-
-        const layers = layersSelector(store.getState())
-        const viewerObservable = Rx.Observable.of(
-            ...layers.filter( layer => layer.type === 'wms').map( layer => 
-                updateNode(
-                    layer.id, 
-                    'layers', 
-                    { featureInfo: featureInfo }
-                )
+    const layers = layersSelector(store.getState())
+    const viewerObservable = Rx.Observable.of(
+        ...layers.filter(layer => layer.type === 'wms').map(layer =>
+            updateNode(
+                layer.id,
+                'layers',
+                { featureInfo: featureInfo }
             )
+        )
+    );
+
+    // if featureInfo or report is already displayed, force viewer refresh by simulating click 
+    const clickPoint = store.getState().mapInfo.clickPoint;
+    if (clickPoint) {
+        return Rx.Observable.merge(
+            viewerObservable,
+            Rx.Observable.of(updateFeatureInfoClickPoint(clickPoint))
         );
+    }
+    else {
+        return viewerObservable;
+    }
+})
 
-        // if featureInfo or report is already displayed, force viewer refresh by simulating click 
-        const clickPoint = store.getState().mapInfo.clickPoint;
-        if (clickPoint) {
-            return Rx.Observable.merge(
-                viewerObservable,
-                Rx.Observable.of(updateFeatureInfoClickPoint(clickPoint))
-            );
-        }
-        else {
-            return viewerObservable;
-        }
-    })
-
-export default {fetchSchemasEpic, displayFormEpic}
+export default { fetchSchemasEpic, displayFormEpic }
